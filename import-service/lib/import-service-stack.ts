bfdfec5224cc232b5e325e80c0e1dabb128cf130
Ilhom Maksadkulov <ilhommaksadkulov@Ilhoms-MacBook-Pro.local>
2024-07-10 16:04:39 +0500

    fix: give permission to queue

--- import-service/lib/import-service-stack.ts
+++ import-service/lib/import-service-stack.ts
@@ -19,12 +19,6 @@ export class ImportServiceStack extends cdk.Stack {
       "my-import-bucket-uz",
     );
 
-    // // Define the SQS queue
-    // const queue = new sqs.Queue(this, 'CatalogItemsQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(30),
-    //   receiveMessageWaitTime: cdk.Duration.seconds(20),
-    // });
-
     // Reference the SQS queue created in ProductServiceStack
     const queue = sqs.Queue.fromQueueArn(this, 'CatalogItemsQueue', cdk.Fn.importValue('CatalogItemsQueueService'));
 
@@ -77,6 +71,9 @@ export class ImportServiceStack extends cdk.Stack {
 
     // Grant the Lambda function permissions to read from the S3 bucket
     bucket.grantReadWrite(importFileParserLambda);
+
+     // Grant permissions to send messages to the SQS queue
+    queue.grantSendMessages(importFileParserLambda);
 
     // Add S3 event notification to trigger the Lambda function
     bucket.addEventNotification(s3.EventType.OBJECT_CREATED, new s3n.LambdaDestination(importFileParserLambda), {
